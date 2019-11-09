448d7f28652e265ac0399c737cd2d0faa75ea224
Luis Brito <nottluis@gmail.com>
2019-11-09 20:01:58 -0300

    Added styles

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 const QuoteBox = (props) => (
     <div id='quote-box'>
         <h1>Random Quote Generator</h1>
-        <h2 id='text'>
-            {props.currentQuote.quote}
-        </h2>
-        <h2 id='author'>
-            {props.currentQuote.author}
-        </h2>
+        <div className="quote">
+            <h2 id='text'>
+                "{props.currentQuote.quote}"
+            </h2>
+            <h2 id='author'>
+                - {props.currentQuote.author}
+            </h2>
+        </div>
+        
         <div className="controls">
-            <button id="new-quote" onClick={props.newQuoteClick}>New quote</button>
+            <button id="new-quote" className="btn" onClick={props.newQuoteClick}>New quote</button>
             <a href={'https://twitter.com/intent/tweet?text=' + props.currentQuote.quote + ' - ' + props.currentQuote.author} target="_blank" id='tweet-quote'>
-                <button id="twitter">Tweet Quote</button>
+                <button id="twitter" className="btn">Tweet Quote</button>
             </a>
         </div>
     </div>
